0a549a3f7146e64d07b4cd827b48fedd6650b75b
jxwolstenholme <jxwolstenholme@gmail.com>
2018-10-03 17:05:16 +0100

    upload setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bt_smarthub_devicelist',
+      version='0.1',
+      description='Retrieves a list of devices from a bt smarthub on a local network',
+      url='https://github.com/jxwolstenholme/bt_smarthub_devicelist',
+      author='Jamie Wolstenholme',
+      author_email='jxwolstenholme@gmail.com',
+      license='MIT',
+      packages=['bt_smarthub_devicelist'],
+      keywords=['bt', 'smarthub', 'devicelist', 'router'],
+      classifiers=['Programming Language :: Python :: 3'])
