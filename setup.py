970b560a2f924526941f5a32eff313c0192e9fde
jxwolstenholme <jxwolstenholme@gmail.com>
2018-12-03 11:32:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='btsmarthub_devicelist',
-    version='0.1',
+    version='0.1.2',
     packages=['btsmarthub_devicelist'],
     url='https://github.com/jxwolstenholme/bt_smarthub_devicelist',
     license='MIT',
