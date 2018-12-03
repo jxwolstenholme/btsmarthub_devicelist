63a03f486551bda2e613048ae29a176b256bc999
jxwolstenholme <jxwolstenholme@gmail.com>
2018-12-03 11:31:43 +0000

    Update __init__.py

--- bt_smarthub_devicelist/__init__.py
+++ bt_smarthub_devicelist/__init__.py
@@ -2,5 +2,5 @@ from btsmarthub_devicelist import get_devicelist
 
 __name__ = "btsmarthub_devicelist"
 __author__ = "Jamie Wolstenholme"
-__version__ = "0.1"
+__version__ = "0.1.2"
 __license__ = "MIT"
