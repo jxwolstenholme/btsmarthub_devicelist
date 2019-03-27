0506969ec7541034919f214f1066722142ba9546
jxwolstenholme <jxwolstenholme@gmail.com>
2019-03-27 14:25:34 +0000

    Delete __init__.py

--- bt_smarthub_devicelist/__init__.py
+++
@@ -1,7 +0,0 @@
-name= "btsmarthub_devicelist"
-from .btsmarthub_devicelist import get_devicelist
-
-__name__ = "btsmarthub_devicelist"
-__author__ = "Jamie Wolstenholme"
-__version__ = "0.1.3"
-__license__ = "MIT"
