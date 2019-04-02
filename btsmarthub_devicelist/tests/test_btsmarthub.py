8f2bb9fd15f1c6ddf14076a047a45b408e3af52a
jxwolstenholme <jxwolstenholme@gmail.com>
2019-04-02 12:16:31 +0100

    Delete test_btsmarthub.py

--- btsmarthub_devicelist/tests/test_btsmarthub.py
+++
@@ -1,7 +0,0 @@
-import btsmarthub_devicelist
-
-def test_devicelist():
-    devicelist = btsmarthub_devicelist.get_devicelist(
-        router_ip='192.168.1.254', only_active_devices=True
-    )
-    assert len(devicelist) > 0
