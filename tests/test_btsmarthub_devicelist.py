58f16f95fc5140eebf6946a5d80ae62f81880066
Paul Goulbourn <paul.goulbourn@cognitran.com>
2020-03-07 12:00:56 +0000

    Added partially redacted test data from a smarthub2 router. Tests now use data from fixtures/smarthub2, with appropriate assertions for counts etc. Test suite should now be possible to run without owning a smarthub2 router.

--- tests/test_btsmarthub_devicelist.py
+++ tests/test_btsmarthub_devicelist.py
@@ -12,32 +12,84 @@ _LOGGER = logging.getLogger(__name__)
 
 class TestBTSmartHub(unittest.TestCase):
 
+    # example bodies from smart hub 2 requests so we can test away from physical/network devices
+    smarthubb2_cgi_owl_body="";
+    smarthubb2_cgi_basicMyDevice = "";
+
+    @classmethod
+    def setUpClass(self):
+        """ Read some faked data into body strings so we can test without a router present"""
+        with open('fixtures/smarthub2/cgi_owl.js', 'r') as file:
+            self.smarthubb2_cgi_owl_body = file.read()
+        with open('fixtures/smarthub2/cgi_basicMyDevice.js', 'r') as file:
+            self.smarthubb2_cgi_basicMyDevice = file.read()
+
+    def setup_fake_smarthub2(self):
+        # initialise mock - make sure smarthub 2 is ok....
+        responses.add(responses.GET, 'http://smarthub2fakedrouter/cgi/cgi_basicMyDevice.js', status=200)
+        responses.add(responses.GET, 'http://smarthub2fakedrouter/cgi/cgi_owl.js', body=self.smarthubb2_cgi_owl_body,
+                      status=200)
+        responses.add(responses.GET, 'http://smarthub2fakedrouter/cgi/cgi_basicMyDevice.js',
+                      body=self.smarthubb2_cgi_basicMyDevice, status=200)
+
+
+    @responses.activate
     def test_btsmarthub2_getdevicelist(self):
-    # self.asserttrue(btsmarthub2_devicelist.detect_smart_hub2('192.168.1.254', 0.5))
-        connected_devices = BTSmartHub(router_ip='192.168.1.254').get_devicelist(only_active_devices=True)
-        all_devices = BTSmartHub(router_ip='192.168.1.254').get_devicelist(only_active_devices=False)
+        self.setup_fake_smarthub2()
+
+        connected_devices = BTSmartHub(router_ip='smarthub2fakedrouter').get_devicelist(only_active_devices=True)
+        all_devices = BTSmartHub(router_ip='smarthub2fakedrouter').get_devicelist(only_active_devices=False)
         self.assertGreaterEqual(len(all_devices), len(connected_devices))
-        for device in connected_devices:
-            _LOGGER.error(
-                          device.get("UserHostName")+"("+device.get("PhysAddress")+") on "+
-                          device.get("IPAddress")+" via "+device.get("ConnectionType")+" through parent "+
-                          device.get("ParentName")+"("+device.get("ParentPhysAddress")+")")
+
+        self.assertEqual(8, len(connected_devices))
+        self.assertEqual(12, len(all_devices))
+
+        # for device in connected_devices:
+        #     _LOGGER.error(
+        #                   device.get("UserHostName")+"("+device.get("PhysAddress")+") on "+
+        #                   device.get("IPAddress")+" via "+device.get("ConnectionType")+" through parent "+
+        #                   device.get("ParentName")+"("+device.get("ParentPhysAddress")+")")
 
 
+    @responses.activate
     def test_disk_dictionary(self):
-        disks = BTSmartHub(router_ip='192.168.1.254').get_disks();
-        for disk in disks:
-            _LOGGER.error(disk + " - " + disks[disk])
+        self.setup_fake_smarthub2()
 
+        disks = BTSmartHub(router_ip='smarthub2fakedrouter').get_disks();
+        # test data has 2 disks and a router.
+        self.assertEqual(3,len(disks))
+
+    @responses.activate
     def test_stations_load(self):
-        stations=BTSmartHub(router_ip='192.168.1.254').get_stations();
+        self.setup_fake_smarthub2()
+
+        stations=BTSmartHub(router_ip='smarthub2fakedrouter').get_stations();
+
+        # test data has known count of stations...
+        self.assertEqual(25,len(stations))
+
+        # grab sone counts to test connection detection
+        ether_count=0;
+        g2_count=0;
+        g5_count=0;
+
         for key in stations:
-            _LOGGER.error("Name "+stations[key].get("station_name")+"("+stations[key].get("station_mac")+") via "+stations[key].get("connect_type")+" ("+stations[key].get("parent_id")+")")
+        #    _LOGGER.error("Name "+stations[key].get("station_name")+"("+stations[key].get("station_mac")+") via "+stations[key].get("connect_type")+" ("+stations[key].get("parent_id")+")")
+            if stations[key].get("connect_type") == "Ether" :
+                ether_count=ether_count+1
+            elif  stations[key].get("connect_type") == "2.4G" :
+                g2_count=g2_count+1
+            elif stations[key].get("connect_type") == "5G":
+                g5_count=g5_count+1
+
+        self.assertEqual(8,ether_count)
+        self.assertEqual(7,g2_count)
+        self.assertEqual(10,g5_count)
+
 
     @responses.activate
     def test_btsmarthub2_with_mocked_smarthub2_present(self):
-        # initialise mock - make sure smarthub 2 is ok....
-        responses.add(responses.GET, 'http://smarthub2fakedrouter/cgi/cgi_basicMyDevice.js', status=200)
+        self.setup_fake_smarthub2()
         # initialise mock - make sure smarthub 2 fails
         responses.add(responses.GET, 'http://smarthub2fakedrouter/gui/#/home/myNetwork/devices', status=400)
 
