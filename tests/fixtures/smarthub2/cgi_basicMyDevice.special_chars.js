21f47a66dba09f241d42eae1b21f39a31f25b369
Matthew Simpson <matthew@matthewsimpson.net>
2022-09-28 16:25:47 +0100

    Handle special characters in Device names
    This seems to be specifically when a single quote (`'`) was used in a
    device name. The JS was being unquoted before tidied up to be parsed as
    JSON, which led to the single quote being converted to a double quote.
    This broke the string as invalid JSON.

---
+++ tests/fixtures/smarthub2/cgi_basicMyDevice.special_chars.js
@@ -0,0 +1,6 @@
+var known_device_list=[
+    {mac:'DC%3AA4%3AFF%3AFF%3AFF%3AFF',hostname:'PaulGousiPadPro',ip:'10%2E1%2E8%2E201',ipv6:'2a00',name:'Paul%27s%20TV',activity:'1',os:'iOS',device:'iPad',time_first_seen:'2019%2F01%2F23%2021%3A08%3A40',time_last_active:'2020%2F03%2F06%2010%3A31%3A08',dhcp_option:'XX',port:'NA',ipv6_ll:'fe80',activity_ip:'1',activity_ipv6_ll:'1',activity_ipv6:'1',device_oui:'NA',device_serial:'NA',device_class:'NA',reconnected:'0'},
+null];
+
+# rest of file truncated for test
+
